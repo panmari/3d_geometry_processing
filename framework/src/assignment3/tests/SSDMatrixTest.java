3db799a2b2f870c697d0c615ddfe56c4f8fa1767
panmari <smmuzi@gmail.com>
2013-10-13 20:56:30 +0200

    moved and cleaned up tests for SSDMatrix

--- framework/src/assignment3/tests/SSDMatrixTest.java
+++ framework/src/assignment3/tests/SSDMatrixTest.java
@@ -1,4 +1,4 @@
-package assignment3;
+package assignment3.tests;
 
 import static org.junit.Assert.*;
 
@@ -17,23 +17,25 @@ import sparse.CSRMatrix;
 import sparse.CSRMatrix.col_val;
 import assignment2.HashOctree;
 import assignment2.HashOctreeVertex;
+import assignment3.SSDMatrices;
 
 public class SSDMatrixTest {
 
 	private PointCloud pc;
 	private HashOctree tree;
-	private CSRMatrix mat;
+	private CSRMatrix D_0, D_1;
 
 	@Before
 	public void setUp() throws IOException{
 		pc = PlyReader.readPointCloud("./objs/octreeTest2.ply", true);
 		tree = new HashOctree(pc,4,1,1f);
-		mat = SSDMatrices.D0Term(tree, pc);
+		D_0 = SSDMatrices.D0Term(tree, pc);
+		D_1 = SSDMatrices.D1Term(tree, pc);
 	}
 	
 	@Test
 	public void D0RowShouldSumUpToOne() {
-		for (ArrayList<col_val> row: mat.rows) {
+		for (ArrayList<col_val> row: D_0.rows) {
 			float sum = 0;
 			for (col_val entry: row) {
 				sum += entry.val;
@@ -43,33 +45,39 @@ public class SSDMatrixTest {
 	}
 	@Test
 	public void D0multipliedByPoints()  {	
-		System.out.println(mat);
 		ArrayList<Point3f> vertexPos = new ArrayList<Point3f>();
 		for (HashOctreeVertex v: tree.getVertices()){
 			vertexPos.add(v.getPosition());
 		}
 		ArrayList<Point3f> result = new ArrayList<Point3f>();
-		assertEquals(tree.getVertices().size(), mat.nCols);
-		mat.multPoints(vertexPos, result);
-		
-		System.out.println("above original vs computed below");
-		
+		assertEquals(tree.getVertices().size(), D_0.nCols);
+		D_0.multPoints(vertexPos, result);		
 
 		for (int i = 0; i < result.size(); i++) {
-			//assertEquals(pc.points.get(i), result.get(i));
-			System.out.println(result.get(i));
-			System.out.println(pc.points.get(i));
-			System.out.println("--------------");
+			Point3f computed = result.get(i);
+			Point3f original = pc.points.get(i);
+			assertTrue(original.distance(computed) < 0.00001f);
 		}
+	}
+	
+	@Test
+	public void D1shouldHaveRightSize() {
+		assertEquals(D_1.nCols, tree.getVertices().size());
+		assertEquals(D_1.nRows, pc.points.size()*3);
+	}
+	
+	/**
+	 * Simple test: If f is a samlpled linear function a*x + b*y + c*z,
+	 *	D1*f = (a,b,c) on each cell.  
+	 */
+	public void petersD1Test() {
+		
 	}
 	
 	@Test
 	public void marchableStuffTest() {
 		//TODO: is 
-		//c.getCornerElement(i, tree).getIndex()
-		//same as
-		//c.getIndex() + i
-		//??
+		//actuall, just look at the output...
 	}
 
 }
