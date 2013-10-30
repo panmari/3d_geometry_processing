0b04f4e7b6b6d4c8bdbc705ed8d14de268ab947a
panmari <smmuzi@gmail.com>
2013-10-30 11:39:04 +0100

    first sketch of smoothing demo

--- framework/src/assignment4/Assignment4_2_smoothing.java
+++ framework/src/assignment4/Assignment4_2_smoothing.java
@@ -1,5 +1,22 @@
 package assignment4;
 
+import glWrapper.GLHalfedgeStructure;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.vecmath.Tuple3f;
+import javax.vecmath.Vector3f;
+
+import meshes.HalfEdgeStructure;
+import meshes.WireframeMesh;
+import meshes.exception.DanglingTriangleException;
+import meshes.exception.MeshNotOrientedException;
+import meshes.reader.ObjReader;
+import openGL.MyDisplay;
+import sparse.CSRMatrix;
+import assignment3.SSDMatrices;
+
 
 /**
  * Smoothing
@@ -8,6 +25,37 @@ package assignment4;
  */
 public class Assignment4_2_smoothing {
 
-	//implement the implicit smoothing scheme
-
+	public static void main(String[] args) throws IOException, MeshNotOrientedException, DanglingTriangleException {
+		WireframeMesh mesh = ObjReader.read("objs/bunny.obj", false);
+		HalfEdgeStructure hs = new HalfEdgeStructure();
+		hs.init(mesh);
+		
+		MyDisplay d = new MyDisplay();
+		CSRMatrix mMixed = LMatrices.mixedCotanLaplacian(hs);
+		CSRMatrix mUniform = LMatrices.uniformLaplacian(hs);
+		CSRMatrix[] laplacians = new CSRMatrix[]{ mUniform, mMixed };
+		for (CSRMatrix m: laplacians) {
+			//CSRMatrix[] laplacians = new CSRMatrix[]{ mUniform, mMixed };
+			float lambda = 0.1f;
+			CSRMatrix I = SSDMatrices.eye(hs.getVertices().size(), hs.getVertices().size());
+			m.scale(-lambda);
+			CSRMatrix smoothM = new CSRMatrix(0,0);
+			smoothM.add(I, m);
+			ArrayList<Tuple3f> smoothedVertices = new ArrayList<Tuple3f>();
+			LMatrices.mult(smoothM, hs, smoothedVertices);
+			GLHalfedgeStructure glHsSmooth = new GLHalfedgeStructure(hs);
+			glHsSmooth.add(smoothedVertices, "position");
+			glHsSmooth.configurePreferredShader("shaders/trimesh_flat.vert",
+					"shaders/trimesh_flat.frag", 
+					"shaders/trimesh_flat.geom");
+			d.addToDisplay(glHsSmooth);
+		}
+		
+		//add initial mesh
+		GLHalfedgeStructure glHs = new GLHalfedgeStructure(hs);
+		glHs.configurePreferredShader("shaders/trimesh_flat.vert",
+				"shaders/trimesh_flat.frag", 
+				"shaders/trimesh_flat.geom");
+		d.addToDisplay(glHs);
+	}
 }
