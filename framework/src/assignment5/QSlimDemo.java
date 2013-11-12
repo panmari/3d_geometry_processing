32fdf8b45a491743c7a4aceb24c331e7e6dd7126
panmari <smmuzi@gmail.com>
2013-11-12 13:48:18 +0100

    added visualisation of edge-to-be-collapsed

--- framework/src/assignment5/QSlimDemo.java
+++ framework/src/assignment5/QSlimDemo.java
@@ -1,7 +1,13 @@
 package assignment5;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
+import javax.vecmath.Color3f;
+
 import glWrapper.GLHalfedgeStructure;
 import openGL.MyDisplay;
+import meshes.HalfEdge;
 import meshes.HalfEdgeStructure;
 import meshes.WireframeMesh;
 import meshes.reader.ObjReader;
@@ -14,7 +20,16 @@ public class QSlimDemo {
 		hs.init(wf);
 		GLHalfedgeStructure untouched = new GLHalfedgeStructure(hs);
 		HalfEdgeCollapse hec = new HalfEdgeCollapse(hs);
-		hec.collapseEdgeAndDelete(hs.getHalfEdges().get(0));
+		HalfEdge h = hs.getHalfEdges().get(0);
+		
+		//mark the halfedge on untouched object
+		ArrayList<Color3f> color = new ArrayList<Color3f>(Collections.nCopies(untouched.getNumberOfVertices(), new Color3f(0,1,0)));
+		color.set(h.end().index, new Color3f(1,0,0));
+		color.set(h.start().index, new Color3f(1,1,0));
+		
+		if (!HalfEdgeCollapse.isEdgeCollapsable(h))
+			throw new RuntimeException();
+		hec.collapseEdgeAndDelete(h);
 		
 		MyDisplay d = new MyDisplay();
 		GLHalfedgeStructure glHs = new GLHalfedgeStructure(hs);
@@ -23,10 +38,11 @@ public class QSlimDemo {
 				"shaders/trimesh_flat.geom");
 		glHs.setName("reduced edges");
 		d.addToDisplay(glHs);
-		
-		untouched.configurePreferredShader("shaders/trimesh_flat.vert",
-				"shaders/trimesh_flat.frag", 
-				"shaders/trimesh_flat.geom");
+
+		untouched.configurePreferredShader("shaders/trimesh_flatColor3f.vert",
+				"shaders/trimesh_flatColor3f.frag", 
+				"shaders/trimesh_flatColor3f.geom");
+		untouched.add(color, "color");
 		untouched.setName("All edges");
 		d.addToDisplay(untouched);
 	}
