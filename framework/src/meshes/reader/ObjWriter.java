7f8548836c24e7ab392a5ee848db5ee6eb620a39
panmari <smmuzi@gmail.com>
2013-12-16 13:48:12 +0100

    took obj writer from gidoca, thx!

--- framework/src/meshes/reader/ObjWriter.java
+++ framework/src/meshes/reader/ObjWriter.java
@@ -2,14 +2,13 @@ package meshes.reader;
 
 import glWrapper.GLWireframeMesh;
 
-import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
 
+import javax.vecmath.Point2f;
 import javax.vecmath.Point3f;
-
-import openGL.MyDisplay;
 
 import meshes.Face;
 import meshes.HalfEdgeStructure;
@@ -17,12 +16,18 @@ import meshes.Vertex;
 import meshes.WireframeMesh;
 import meshes.exception.DanglingTriangleException;
 import meshes.exception.MeshNotOrientedException;
+import openGL.MyDisplay;
 
 public class ObjWriter {
+	FileWriter fw;
+	
+	public ObjWriter(String file) throws IOException
+	{
+		fw = new FileWriter("./out/"+file);
+	}
 
-	public static void write(WireframeMesh m, String file) throws IOException{
+	public void write(WireframeMesh m) throws IOException{
 		
-		FileWriter fw = new FileWriter("./out/"+file);
 		for(Point3f v: m.vertices){
 			fw.write("v " + v.x + " " + v.y + " " + v.z + "\n");
 		}
@@ -30,12 +35,9 @@ public class ObjWriter {
 		for(int[] f : m.faces){
 			fw.write("f " + (f[0] + 1) + " " + (f[1]+1) + " " + (f[2]+1) + "\n");
 		}
-		
-		fw.close();
 	}
 	
-	public static void write(HalfEdgeStructure hs, String file) throws IOException {
-		FileWriter fw = new FileWriter("./out/"+file);
+	public void write(HalfEdgeStructure hs) throws IOException {
 		hs.enumerateVertices();
 		for(Vertex v: hs.getVertices()){
 			fw.write("v " + v.getPos().x + " " 
@@ -47,25 +49,19 @@ public class ObjWriter {
 		
 			Iterator<Vertex> it = f.iteratorFV();
 			int[] fc = {it.next().index, it.next().index, it.next().index};
-			fw.write("f " + (fc[0] + 1) + " " + (fc[1]+1) + " " + (fc[2]+1) + "\n");
+			fw.write("f " + (fc[0] + 1) + "/" + (fc[0] + 1) + " " + (fc[1]+1) + "/" + (fc[1] + 1) + " " + (fc[2]+1) + "/" + (fc[2] + 1) + "\n");
 		}
-		
-		fw.close();
 	}
 	
-	public static void main(String[] args) throws IOException, MeshNotOrientedException, DanglingTriangleException{
-		WireframeMesh m = ObjReader.read("objs/bunny.obj", true);
-		
-		HalfEdgeStructure hs = new HalfEdgeStructure();
-		hs.init(m);
-		write(hs, "bla.obj");
-		
-		m = ObjReader.read("out/bla.obj", true);
-		
-		GLWireframeMesh glwf = new GLWireframeMesh(m);
-		MyDisplay d = new MyDisplay();
-		d.addToDisplay(glwf);
+	public void writeTexcoord(ArrayList<Point2f> texcoords) throws IOException {
+		for(Point2f tc: texcoords)
+		{
+			fw.write("vt " + String.format("%f", tc.x) + " " + String.format("%f", tc.y) + " 0\n");
+		}
 	}
-
-
+	
+	public void close() throws IOException
+	{
+		fw.close();
+	}
 }
